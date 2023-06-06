e4177f8f7adfa49ae146c48bae6a1a8a02f6c788
Nathaniel <129980776+nbdpdx@users.noreply.github.com>
2023-06-06 09:08:31 -0700

    Create u5a1ArrayList.java

---
+++ u5a1ArrayList.java
@@ -0,0 +1,22 @@
+package u5a1.array.list;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author douglassfamily
+ */
+public class U5A1ArrayList {
+
+    public static void main(String[] args) {
+        // Make ArrayList of 4 favorite foods
+        ArrayList favoriteFoods = new ArrayList();
+        favoriteFoods.add("Sushi");
+        favoriteFoods.add("Coconuts");
+        favoriteFoods.add("Macadamia Nuts");
+        favoriteFoods.add("Shave Ice");
+        System.out.println(favoriteFoods);
+        
+    }
+    
+}
